09ea7c8b82658e118ba3873beefb6b9719bbc520
Robert Dutra <robertdutra10@hotmail.com>
2023-10-23 18:47:21 -0300

    Utilizando a anotação @Operation para descrever os endpoints.

--- src/main/java/com/picpaysimplificado/controller/UserController.java
+++ src/main/java/com/picpaysimplificado/controller/UserController.java
@@ -4,6 +4,7 @@ import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dto.UserDTO;
 import com.picpaysimplificado.exceptions.EntityNotFoundException;
 import com.picpaysimplificado.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,30 +21,36 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Serviço para cadastrar usuário.")
     public User save(@RequestBody @Valid UserDTO user) {
         return service.create(user);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
+    @Operation(summary = "Serviço para buscar usuário por id.")
     public User findByid(@PathVariable Long id) throws EntityNotFoundException {
         return service.findUserById(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
+    @Operation(summary = "Serviço para atualizar usuário por id.")
+
     public UserDTO update(@PathVariable Long id, @RequestBody @Valid UserDTO user) throws EntityNotFoundException {
         return service.update(id, user);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Serviço para listar usuários.")
     public List<User> findAll() {
         return service.findAll();
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Serviço para deletar usuário por id.")
     void delete(@PathVariable Long id) throws EntityNotFoundException {
         service.delete(id);
     }
